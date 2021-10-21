28af33281aa63ab8da88271b133d89f29dfbb922
israxx2 <israxx2@gmail.com>
2021-10-21 00:05:56 -0300

    Favorite Page

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,34 +1,40 @@
 import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import "../assets/styles/components/FavoriteList.scss";
 import Champion from "./Champion";
 import Empty from "./Empty";
+import { Container, Row } from "react-bootstrap";
+
+import { useSelector } from "react-redux";
+import { selectChampion } from "../features/favorite/favoritesSlice";
 
 const FavoriteList = (props) => {
-  //const { favoriteCharacters } = props;
+  const favoriteChampions = useSelector(selectChampion);
 
-//   return (
-//     <div className="characterList">
-//       {!favoriteCharacters.length ? (
-//         <Empty />
-//       ) : (
-//         favoriteCharacters.map((character) => (
-//           <Champion key={character.data.id} data={character.data} />
-//         ))
-//       )}
-//     </div>
-//   );
-
-return (
-    <h1>hola mundo</h1>
-    );
+  return (
+    <div className="characterList">
+      {!favoriteChampions.length ? (
+        <Empty />
+      ) : (
+        <Container fluid="md">
+          <Row xm={1} xs={3} md={5}>
+            {favoriteChampions.map((champion) => (
+              <Champion key={champion.id} data={champion} />
+            ))}
+          </Row>
+        </Container>
+      )}
+    </div>
+  );
 };
 
 // native react-redux functions vs hooks: useSelect && useDispatch
-const mapStateToProps = (state) => {
-  return {
-    favoriteCharacters: state.favoriteCharacters,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     favoriteCharacters: state.favoriteCharacters,
+//   };
+// };
 
-export default connect(mapStateToProps, null)(FavoriteList);
+// export default connect(mapStateToProps, null)(FavoriteList);
+
+export default FavoriteList;
